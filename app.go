3a3ca01f5de7813201e37a626aab1b1e4efd5158
mursts <mursts@gmail.com>
2020-03-14 16:00:16 +0900

    add appengine files

---
+++ app.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	 "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", handler)
+
+	port := os.Getenv("port")
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(":"+port, nil)
+}
